e94a07d11eae79a8aea30da7d8a876d6b7560ee4
gcoter <guillaume.coter@gmail.com>
2024-03-24 21:17:13 +0100

    Add a command to import cards from decks

--- mtgc/cli/card.py
+++ mtgc/cli/card.py
@@ -48,12 +48,12 @@ class CardCLI:
         }
 
     def __get_card_path(self, card_json_dict):
-        card_filename = self.__get_card_filename(card_json_dict)
+        card_filename = self.__get_card_filename(card_name=card_json_dict["scryfall"]["name"])
         card_path = os.path.join(self.card_data_folder, f"{card_filename}.json")
         return card_path
 
-    def __get_card_filename(self, card_json_dict):
-        card_name = card_json_dict["scryfall"]["name"]
+    def __get_card_filename(self, card_name: str):
+        card_name = card_name.replace("/", " - ")
         return card_name
 
     def __handle_existing_card(self, card_json_dict, card_path):
@@ -79,3 +79,29 @@ class CardCLI:
     def __save_card(self, card_json_dict, path):
         with open(path, "w") as f:
             json.dump(card_json_dict, f, indent=4)
+
+    def add_from_deck(self, deck: str, output_folder_path: str):
+        deck_json_dict = self.__load_deck(deck)
+        n_deck_cards = len(deck_json_dict["cards"])
+        for n, deck_card_json in enumerate(deck_json_dict["cards"]):
+            print(f"Add card {n+1}/{n_deck_cards}")
+            card_name = deck_card_json["name"]
+            card_filename = self.__get_card_filename(card_name)
+            card_path = os.path.join(output_folder_path, f"{card_filename}.json")
+            if not os.path.exists(card_path):
+                card_json_dict = self.__search_for_card(query=card_name)
+                self.__save_card(card_json_dict, path=card_path)
+                card_name = card_json_dict["scryfall"]["name"]
+                print(f"Added card '{card_name}' to collection ('{card_path}')")
+
+    def __load_deck(self, path):
+        with open(path, "r") as f:
+            deck_json_dict = json.load(f)
+        return deck_json_dict
+
+    def add_from_decks(self, deck_folder: str, output_folder_path: str):
+        deck_filenames = os.listdir(deck_folder)
+        for n, filename in enumerate(deck_filenames):
+            print(f"===== Add cards from deck {n+1}/{len(deck_filenames)} =====")
+            deck_file_path = os.path.join(deck_folder, filename)
+            self.add_from_deck(deck=deck_file_path, output_folder_path=output_folder_path)
